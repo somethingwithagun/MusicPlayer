95432ea104034cd9686b14d3e4c8650f207ec2d8
somethingwithagun <112172412+somethingwithagun@users.noreply.github.com>
2023-03-12 19:12:11 +0300

    deleted mess2

--- debug/CMakeFiles/MusicPlayer.dir/MusicPlayer_autogen/EWIEGA46WW/qrc_mainwindowRes.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MusicPlayer.dir/MusicPlayer_autogen/EWIEGA46WW/qrc_mainwindowRes.cpp.obj: \
- D:\my-folder\documents\projects\C++\dev\MusicPlayer\debug\MusicPlayer_autogen\EWIEGA46WW\qrc_mainwindowRes.cpp
--- debug/CMakeFiles/MusicPlayer.dir/MusicPlayer_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MusicPlayer.dir/MusicPlayer_autogen/mocs_compilation.cpp.obj: \
- D:\my-folder\documents\projects\C++\dev\MusicPlayer\debug\MusicPlayer_autogen\mocs_compilation.cpp
